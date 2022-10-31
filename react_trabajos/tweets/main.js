db05eba6ee6e1bfbf8102a51b28108a604fda61b
Jheison <ruateo555@gmail.com>
2022-10-31 02:16:00 -0400

    tweets terminado

--- react_trabajos/tweets/main.js
+++ react_trabajos/tweets/main.js
@@ -3,10 +3,20 @@ let inputName = document.getElementById('input-name');
 let txtArea =  document.getElementById('txt-area');
 let count = document.getElementById('count');
 let btnPost = document.getElementById('btn-post');
+let btnUpper = document.getElementById('btn-Upper')
+let btnLower = document.getElementById('btn-Lower')
 let containerComments = document.getElementById('container-comments');
+let imgProfile = document.getElementById('img-profile');
+
+
+
 
 let comment;
 let comments = [];
+let quantity = txtArea.textContent.length;
+let lastLetter = "";
+let newTxt = "";
+
 
 function fillcard(element){
     const card = document.createElement('div');
@@ -26,13 +36,64 @@ function fillcard(element){
     containerComments.appendChild(card);
 }
 
-btnPost.addEventListener('click', (e)=>{
-    e.preventDefault();
-    name.textContent = inputName.value;
+function createComment(inputName,txtArea){
     comments = []; 
     comment = {
-        user: inputName.value,
-        text: txtArea.value} 
+        user: inputName,
+        text: txtArea}
+    return comment;
+}
+btnLower.addEventListener('click', (e)=>{
+    e.preventDefault();
+    newTxt = txtArea.value.toLowerCase();
+    txtArea.value = newTxt;
+})
+
+btnUpper.addEventListener('click', (e)=>{
+    e.preventDefault();
+    newTxt = txtArea.value.toUpperCase();
+    txtArea.value = newTxt;
+})
+
+txtArea.addEventListener('keydown', (e)=>{
+    
+
+    if(e.key =='Backspace'){
+        quantity = quantity-1;
+        count.textContent = `Count: ${quantity}`
+
+    }else{
+        quantity++;
+        count.textContent = `Count: ${quantity}`
+    }
+
+    if(quantity < 1){
+       quantity = 0; 
+       count.textContent = `Count: ${quantity}`
+
+    }else if(quantity >= 280){
+        txtArea.style.background = 'red';
+        lastLetter = txtArea.value.slice(0,txtArea.value.length -1);
+        txtArea.value = lastLetter;
+        quantity --;
+ 
+    } else if(quantity >= 230 && quantity <=280){
+        count.style.color ='red';
+    }else if(quantity <280){
+        count.style.color='black'
+        txtArea.style.background = 'white';
+
+    }else{
+        console.log('algo salio mal')
+
+    }
+})
+
+btnPost.addEventListener('click', (e)=>{
+    e.preventDefault();
+    imgProfile.textContent = inputName.value[0];
+    name.textContent = inputName.value;
+    comment = createComment(inputName.value, txtArea.value)    
     comments.push(comment) 
     comments.forEach((element,i)=>{
        fillcard(element) 
